0765fce181c9e6dd42dbf33e4c1bdb866a786910
Nina <you@example.com>
2023-01-24 22:42:38 +0300

    Решение задач 47,50,52

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Напишите программу, которая на вход принимает число, и проверяет есть ли такое число в двумерном массиве, а также возвращает сообщение о том, что оно 
+найдено или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(10, 30);
+if (GetArray2(N, array))
+{ Console.WriteLine($"Число {N} есть в массиве"); }
+else
+{ Console.WriteLine($"Число {N} отсутствует в массиве"); }
+
+
+int[,] GetArray(int minValue, int maxValue)
+{
+    int[,] result = new int[3, 4];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            Console.Write($" {result[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
+
+bool GetArray2(int N, int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == N) return true;
+
+        }
+    }
+    return false;
+}
---
+++ Zadacha52/Program.cs
@@ -0,0 +1,79 @@
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Введите число строк:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число столбцов:");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+ArithmeticArray(array);
+Console.WriteLine($"{String.Join(";", ArithmeticArray(array))}");
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ double[] ArithmeticArray(int[,] array)
+ {
+   double[] sum=new double[columns];
+    for (int j = 0; j < columns; j++)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            sum[j]+=array[i,j];
+            
+        }
+        sum[j]=Math.Round((sum[j]/rows),1);
+    }
+    return sum;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
